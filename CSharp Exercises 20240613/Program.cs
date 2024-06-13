54749702b2265a18d60ac2cb17d70243820e4ead
sheryl acree <sacree@tql.com>
2024-06-13 11:54:55 -0400

    Add project files.

---
+++ CSharp Exercises 20240613/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CSharp_Exercises_20240613;
+
+internal class Program {
+    static void Main(string[] args) {
+        int[] nbrs= {
+           754, 233, 509, 792, 700, 596, 833, 658, 998, 742,
+   187, 754, 308, 914, 489, 867, 717, 586, 929, 467,
+   460, 241, 770, 324, 599, 259, 120, 800, 336, 609,
+   690, 134, 598, 249, 282, 574, 334, 956, 659, 214,
+   435, 643, 809, 874, 906, 620, 328, 369, 426, 561
+    };
+
+        int highest = 99;
+        int lowest = 1000;
+        int sum = 0;
+        foreach (var nbr in nbrs) {
+            if (nbr > highest) {
+                highest = nbr;
+            }
+            if(nbr < lowest){
+                lowest = nbr;
+            }
+            sum += nbr;
+           // sum = sum + nbr;
+        }
+        Console.WriteLine($"Highest: {highest}, Lowest: {lowest}, Average: {sum/ nbrs.Length}");
+    }
+}
